8953cd0d6ff242469b68eed4fb215ced61710834
Ruslan Fedotov <ruslan-fedotov@inbox.ru>
2023-05-01 01:31:20 +0300

    feat: add js logic ver.1

--- script.js
+++ script.js
@@ -10,12 +10,60 @@ textarea.className = 'textarea';
 main.prepend(textarea);
 textarea.focus();
 
-// keybord
-const keybord = document.createElement('div');
-keybord.className = 'keybord';
-main.append(keybord);
+//keyboard
+const keyboard = document.createElement('div');
+keyboard.className = 'keyboard';
+main.append(keyboard);
 
 //information
 const information = document.createElement('div');
 information.className = 'information';
 main.append(information);
+
+
+
+
+let keyboardEng = [
+  ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace"],
+  ["tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "del"],
+  ["caps lock", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "enter"],
+  ["shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "up", "shift"],
+  ["ctrl", "win", "alt", "space", "alt", "left", "down", "right"]
+];
+
+let keyboardRus = [
+  ["ё", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace"],
+  ["tab", "й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ", "del"],
+  ["caps lock", "ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э", "enter"],
+  ["shift", "я", "ч", "с", "м", "и", "т", "ь", "б", "ю", ".", "up", "shift"],
+  ["ctrl", "win", "alt", "space", "alt", "left", "down", "right"]
+];
+
+for (let i = 0; i < keyboardEng.length; i++){	
+	let keyboardRow = document.createElement('div');
+	keyboardRow.className = "keyboard-row";
+	keyboard.append(keyboardRow);
+
+for(let j = 0; j < keyboardEng[i].length; j++){
+	let button = document.createElement('button');
+	button.innerHTML = keyboardEng[i][j];
+	button.className = keyboardEng[i][j] + "-btn" + ' btn';
+	keyboardRow.append(button);
+};
+}
+
+document.onkeydown = function(event) {
+  textarea.innerHTML = event;
+}
+
+let button = document.querySelectorAll('.btn');
+
+ document.onkeydown = function(event) {
+    textarea.innerHTML = event;
+  } 
+
+  button.forEach(item => item.addEventListener('click', function() {
+  if(item.classList.contains('space-btn')) {
+    textarea.innerHTML += ' ';
+  } else { textarea.innerHTML += item.textContent;}
+}));
